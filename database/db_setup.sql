334c48ea66c17c06833fdfa374192194e42ffc84
Jon <jon.mugica@alumni.mondragon.edu>
2019-01-15 13:22:58 +0100

    Changes database 2.0

--- database/db_setup.sql
+++ database/db_setup.sql
@@ -1,19 +1,13 @@
-CREATE USER 'admin'@'localhost' IDENTIFIED BY 'admin';
-GRANT ALL PRIVILEGES ON * . * TO 'admin'@'localhost';
-FLUSH PRIVILEGES;
 drop database if exists amuzon;
 create database amuzon;
 use amuzon;
 
-DROP SCHEMA IF EXISTS Automatic_Distribution_System;
-CREATE SCHEMA IF NOT EXISTS Automatic_Distribution_System DEFAULT CHARACTER SET utf8;
-USE Automatic_Distribution_System;
 create table users(
 userID               TINYINT UNSIGNED,
 name            varchar(25)	 NOT NULL,
 surname            varchar(25)	 NOT NULL,
 userName            varchar(25)	 NOT NULL,
-email            varchar(25)	 NOT NULL,
+email            varchar(50)	 NOT NULL,
 password               char(9)
 );
 
@@ -28,8 +22,9 @@ isManager         boolean
 
 create table vehicles(
 vehicleID  		TINYINT UNSIGNED,
-statee  	VARCHAR(25)     NOT NULL,
-position    VARCHAR(25)     NOT NULL
+statee  	boolean,
+position    VARCHAR(25)     NOT NULL,
+velocity 	decimal(7,2)
 );
 
 create table orders(
@@ -44,18 +39,16 @@ total                decimal(7,2)
 
 
 create table category(
-categoryID  		TINYINT UNSIGNED,
-description  	VARCHAR(25)     NOT NULL
+categoryID  		VARCHAR(25)   NOT NULL
 );
 
 create table product(
 productID               TINYINT UNSIGNED,
-categoryID               TINYINT UNSIGNED,
-description            varchar(25)	 NOT NULL,
+categoryID               VARCHAR(25) NOT NULL,
+description            varchar(35)	 NOT NULL,
 stock            SMALLINT UNSIGNED,
 prize                decimal(7,2),
-warehouseID               TINYINT UNSIGNED,
-isManager         boolean
+warehouseID               TINYINT UNSIGNED
 );
 
 create table orders_product(
@@ -69,27 +62,27 @@ total                decimal(7,2)
 
 ALTER TABLE users 
 	ADD  CONSTRAINT PK_users PRIMARY KEY users(userID);
-
-
+	
+	
 ALTER TABLE warehouseOperator 
 	ADD  CONSTRAINT PK_warehouseOperator PRIMARY KEY warehouseOperator(operatorID);
-
+	
 ALTER TABLE vehicles 
 	ADD  CONSTRAINT PK_vehicles PRIMARY KEY vehicles(vehicleID);
-
+	
 ALTER TABLE orders 
 	ADD  CONSTRAINT PK_orders PRIMARY KEY orders(orderID),
 	ADD CONSTRAINT FK_orders1 FOREIGN KEY orders (userID) REFERENCES users(userID),
 	ADD CONSTRAINT FK_orders2 FOREIGN KEY orders (vehicleID) REFERENCES vehicles(vehicleID);
-
+	
 ALTER TABLE category 
 	ADD  CONSTRAINT PK_category PRIMARY KEY category(categoryID);	
-
-
+	
+	
 ALTER TABLE product 
 	ADD  CONSTRAINT PK_product PRIMARY KEY product(productID),
 	ADD CONSTRAINT FK_product FOREIGN KEY product (categoryID) REFERENCES category(categoryID);
-
+	
 
 ALTER TABLE orders_product 
 	ADD CONSTRAINT PK_orders_product PRIMARY KEY orders_product (orderID,productID),
